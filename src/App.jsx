cd5bf22069b131a009c2ab56f68acc6bce7621f6
munemo <john.chimbani@outlook.com>
2020-09-14 15:41:56 +0200

    Modifying the App.jsx with Form. Creating __test__ folder and Form.spec.js file and modifying it.

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,27 @@ import React, { Component } from 'react'
 
 export class App extends Component {
   render() {
-    return 
-      <div></div>  
+    return (
+      <form>
+        <label htmlFor="weight">Weight</label>
+        <input
+          type="weight"
+          required
+          placeholder="Weight in kgs"
+          name="Weight"
+          id="Weight"
+        />
+        <label htmlFor="height">Height</label>
+        <input
+          type="number"
+          required
+          placeholder="Height in cms"
+          name="height"
+          id="height"
+        />
+        <button>Calculate BMI</button>
+      </form>
+    )
   }
 }
 
